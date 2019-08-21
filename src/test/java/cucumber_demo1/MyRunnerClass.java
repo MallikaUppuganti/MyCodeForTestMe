c6f980600ab04314b5458f9e44fac0a44143d78e
MallikaUppuganti <54269283+MallikaUppuganti@users.noreply.github.com>
2019-08-21 10:28:11 +0530

    Update MyRunnerClass.java

--- src/test/java/cucumber_demo1/MyRunnerClass.java
+++ src/test/java/cucumber_demo1/MyRunnerClass.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="C:\\Users\\training_h2a.06.15\\Desktop\\Testing\\Week 3\\Selenium WorkSpace\\Cucumber_demo\\MyFeatures\\DemoOnTags.feature",glue="cucumber_demo1",tags= {"@Regression,@Smoke"})
-public class MyRunnerClass {
+@CucumberOptions(features="C:\\Users\\training_h2a.06.15\\Desktop\\Testing\\Week 3\\Selenium WorkSpace\\Cucumber_demo\\MyFeatures\\DemoOnTags.feature",glue="cucumber_demo1",tags= {"@Regression,@Smoke"},plugin={"html:target/cucumber-htmlreport.html","json:target/cucumber.json"})
+public class MyRunnerClass 
 
 }
